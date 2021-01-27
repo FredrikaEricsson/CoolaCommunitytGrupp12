3a6ba91c37a18bd2e824b0dac06e04bf5a0743fe
fredrikaericsson <fredrika@finnvid.se>
2021-01-27 10:37:53 +0100

    Added test: Cant post if textarea is empty

--- cypress/integration/guestbook.js
+++ cypress/integration/guestbook.js
@@ -1,12 +1,16 @@
 describe("guestbook", () => {
-  it("add/remove entry", () => {
+  it("Can add/remove entry", () => {
     cy.visit("/guestbook.html");
     cy.get("form textarea").type("Hej här är ett inlägg");
     cy.get("form").submit();
     cy.get("#entries p a").click().end();
   });
 
-  //it("add new entry", () => {});
+  it("Cant post if textarea is empty", () => {
+    cy.visit("/guestbook.html");
+    cy.get("form textarea").should("have.value", "");
+    cy.get("form").submit().end();
+  });
 
   //it("add new entry", () => {});
 
