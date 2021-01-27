97f3ab65b9f2476197436286b17ebc9e6691770c
fredrikaericsson <fredrika@finnvid.se>
2021-01-27 11:36:44 +0100

    Added test: Cant post less than 3 characters

--- cypress/integration/guestbook.js
+++ cypress/integration/guestbook.js
@@ -9,10 +9,21 @@ describe("guestbook", () => {
   it("Cant post if textarea is empty", () => {
     cy.visit("/guestbook.html");
     cy.get("form textarea").should("have.value", "");
-    cy.get("form").submit().end();
+    cy.get("form").submit();
+    cy.get("form h3").contains("För kort meddelande!").end();
   });
 
-  //it("add new entry", () => {});
-
-  //it("add new entry", () => {});
+  it("Cant post less than 3 characters", () => {
+    cy.visit("/guestbook.html");
+    cy.get("form textarea").type("H");
+    cy.get("form").submit();
+    cy.get("form h3").contains("För kort meddelande!");
+    cy.get("form textarea").type("E");
+    cy.get("form").submit();
+    cy.get("form h3").contains("För kort meddelande!");
+    cy.get("form textarea").type("J");
+    cy.get("form").submit();
+    cy.get("#entries p").contains("HEJ");
+    cy.get("#entries p a").click().end();
+  });
 });
