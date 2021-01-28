08cdf76f6042840b07f8123346c9eb6847cdc03e
erik åström <erik.astrom2@gmail.com>
2021-01-28 12:34:34 +0100

    final walkthrough commit

--- cypress/integration/walkthrough.js
+++ cypress/integration/walkthrough.js
@@ -1,3 +1,4 @@
+
 describe('User walkthrough website', ()=>{
 
     it('Can sign in', ()=>{
@@ -5,10 +6,13 @@ describe('User walkthrough website', ()=>{
         cy.get('form');
         cy.get('input[name="username"]').type("CoolUser").should('have.value', 'CoolUser')
         cy.get('input[name="password"]').type('123123123').should('have.value', '123123123' )
-        cy.get('form').submit(); // kör formuläret
-        cy.url().should('include', 'start'); // kolla att url:en innehåller start
-        cy.contains('Välkommen CoolUser') // kolla att hemsidan innehåller följande string
+        cy.get('form').submit(); 
+        cy.url().should('include', 'start'); 
+        cy.contains('Välkommen CoolUser') 
         cy.get('ul>li').eq(2).click();
+    });
+
+    it('Can create user', ()=>{
         cy.get('a').click();
         cy.get('form');
         cy.get('input[name="username"]').type('newuserlogin').should('have.value', 'newuserlogin');
@@ -17,15 +21,21 @@ describe('User walkthrough website', ()=>{
         cy.get('input[name="passwordConfirm"]').type('newuserpassword').should('have.value', 'newuserpassword');
         cy.get('form').submit();
         cy.contains("Anvädaren registrerad!");
+    });
+
+    it('Can login after creating new user', ()=>{
         cy.get('a').click();
         cy.get('form');
         cy.get('input[name="username"]').type("CoolUser").should('have.value', 'CoolUser')
         cy.get('input[name="password"]').type('123123123').should('have.value', '123123123' )
         cy.get('form').submit();
+    });
+
+    it('Can use guestbook', ()=>{
         cy.get('ul>li').eq(0).click();
         cy.contains('Gästbok!');
         cy.get('form');
-        cy.get('textarea').type("Detta är mitt första inlägg, spännande! :)");
+        cy.get('textarea').type("Detta är mitt första inlägg, spännande! :)").should('have.value', "Detta är mitt första inlägg, spännande! :)");
         cy.get('form').submit();
         cy.get('a[href*="start.html"]').click()
         cy.wait(1000);
@@ -37,10 +47,14 @@ describe('User walkthrough website', ()=>{
         cy.get('ul>li').eq(0).click();
         cy.wait(1000);
         cy.get('a').click(); // Ifall det inte finns några inlägg så är "Tillbaka" den ända a taggen = inlägget är borttaget!
+    });
+
+    it('Can use presentation', ()=>{
         cy.get('ul>li').eq(1).click();
         cy.contains('Prentation!');
         cy.get('a').click();
-        
+        cy.get('ul>li').eq(2).click();
     });
+    
 });
 
